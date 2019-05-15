32351f79762479c15137100f80055f0c957c2a8f
bytexplore <bytexplore.official@gmail.com>
2019-05-15 10:26:31 +0700

    website

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-05-03 09:30:11 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-05-03T09:30:11+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-05-03T09:30:16+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-05-03T09:30:22+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
